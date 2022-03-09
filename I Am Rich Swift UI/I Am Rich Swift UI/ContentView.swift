c9fe4153ec8cb7919d28ac9df003a2c77cd5f0a1
nwing <nestorblancog@gmail.com>
2022-03-09 12:25:20 -0400

    Add first swiftUi proyect I'm Rich

---
+++ I Am Rich Swift UI/I Am Rich Swift UI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I Am Rich Swift UI
+//
+//  Created by Nestor on 9/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich Swift UI/I Am Rich Swift UI/I_Am_Rich_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_Swift_UIApp.swift
+//  I Am Rich Swift UI
+//
+//  Created by Nestor on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
