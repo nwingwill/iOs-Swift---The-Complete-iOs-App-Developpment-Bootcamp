4a9723c3a110a161094e25b8d54a7f92018b1ef5
nwing <nestorblancog@gmail.com>
2022-03-09 19:04:42 -0400

    Add Dicee SwiftUI Proyect

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Nestor on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
