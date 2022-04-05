5a666fef29f55b59f82486681f7320880fb80f8e
nwing <nestorblancog@gmail.com>
2022-04-05 19:42:15 -0400

    Add core data examples

---
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by nwing on 31/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
