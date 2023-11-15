2749b646284aa882635666cd5332916e2c07c5cc
nwingwill <nestorblancog@gmail.com>
2023-11-14 22:15:40 -0400

    ➕ADD: Outlet
    ➕ADD Outlet element

--- Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
+++ Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var ifFinishedEditedNumber : Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func calcButtonPresed(_ sender: UIButton) {
+        
+        ifFinishedEditedNumber = true
 
-
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numValue = sender.currentTitle {
+            
+            if ifFinishedEditedNumber {
+                
+                displayLabel.text = numValue
+                ifFinishedEditedNumber = false
+                
+            } else {
+                
+                displayLabel.text = displayLabel.text! + numValue
+                
+            }
+            
+        }
+        
+    }
+    
 }
 
